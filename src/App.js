92f27a1b51d048d071ba5eff3cf3a16078f8febe
Hermmans Orejuela <hermmansorejuela@gmail.com>
2022-10-26 19:15:20 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import './App.css';
+import ProblemsPage from './Components/ProblemsPage';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <ProblemsPage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
